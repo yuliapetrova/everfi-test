6020750c2e8157f75efc225d819ae03d8ce09221
yulia.petrova <yulia.petrova@beable.com>
2021-03-01 00:48:33 -0400

    Refactoring

--- test/external-process.ts
+++ test/external-process.ts
@@ -1,7 +1,7 @@
 import {expect} from "chai";
 import request from "supertest";
 
-describe('Tests against API running as an external process', () => {
+describe('Tests against API running as an external process at localhost:3000/vegetable', () => {
 
     beforeEach(done => setTimeout(done, 500));
 
@@ -34,14 +34,14 @@ describe('Tests against API running as an external process', () => {
         expect(response.body.error).to.be.equal('Missing required fields name, code and price');
     });
 
-    it('should get all vegetable names', async () => {
+    it('should get all product names', async () => {
 
         const response = await request('localhost:3000').get('/vegetable');
         expect(response.status).to.be.equal(200);
         expect(response.body[0]).to.be.equal('apple');
     });
 
-    it('should delete vegetable', async () => {
+    it('should delete a product', async () => {
 
         const response = await request('localhost:3000').delete('/vegetable/apple');
         expect(response.status).to.be.equal(204);
