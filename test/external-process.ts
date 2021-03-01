9ea082b2ed291fe42239593f504ea80edb9f09d8
yulia.petrova <yulia.petrova@beable.com>
2021-03-01 00:07:07 -0400

    Added tests against API running as an external process

--- test/external-process.ts
+++ test/external-process.ts
@@ -3,8 +3,48 @@ import request from "supertest";
 
 describe('', () => {
 
+    beforeEach(done => setTimeout(done, 500));
+
+    it('should add a product', async () => {
+
+        const response = await request('localhost:3000')
+            .post('/vegetable')
+            .set('Content-type', 'application/json')
+            .send({ name: 'apple', code: '12345', price: '100' });
+        expect(response.status).to.be.equal(201);
+    });
+
+    it('should get error message when add a product with existing name', async () => {
+
+        const response = await request('localhost:3000')
+            .post('/vegetable')
+            .set('Content-type', 'application/json')
+            .send({ name: 'apple', code: '12345', price: '100' });
+        expect(response.status).to.be.equal(400);
+        expect(response.body.error).to.be.equal('Product with this name is already exists');
+    });
+
+    it('should get error message when add a product with missing price or code', async () => {
+
+        const response = await request('localhost:3000')
+            .post('/vegetable')
+            .set('Content-type', 'application/json')
+            .send({ name: 'orange', price: '100' });
+        expect(response.status).to.be.equal(400);
+        expect(response.body.error).to.be.equal('Missing required fields name, code and price');
+    });
+
     it('should GET all vegetable names', async () => {
+
         const response = await request('localhost:3000').get('/vegetable');
         expect(response.status).to.be.equal(200);
+        expect(response.body[0]).to.be.equal('apple');
+    });
+
+    it('should DELETE vegetable', async () => {
+
+        const response = await request('localhost:3000').delete('/vegetable/apple');
+        expect(response.status).to.be.equal(204);
+        expect(response.body[0]).to.be.equal('apple');
     });
 });
